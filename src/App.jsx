5f2dad02c21a3f8196201b7934a431936fc1108b
Daoud-Hussain <daoudhussain302@gmail.com>
2023-12-16 12:40:53 +0500

    Routing completed

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
+import Services from './Services';
+import About from './About';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route element={<Home />} path='/' />
+          <Route element={<About />} path='/about' />
+          <Route element={<Contact />} path='/contact' />
+          <Route element={<Services />} path='/services' />
+          <Route path='*' element={<Navigate to="/" />} />
+
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
