c91b3bd40c51315d28bed821b171cf6602dc89ba
Daoud-Hussain <daoudhussain302@gmail.com>
2023-12-18 18:29:50 +0500

    Creating contact component

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,9 +2,34 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact Page</h1>
-        </div>
+        <>
+            <div class="container mt-5 text-lg">
+                <h1>Contact Us</h1>
+                <p> Joining a gym is a decision that can change your life for the better. It's about more than just sculpting your physique; it's about building mental fortitude and developing healthy habits that will serve you for a lifetime. Embrace the challenge, push yourself to new heights, and enjoy the satisfaction of knowing that each day in the gym is another step towards becoming the best version of yourself. So join today, and let the journey begin!</p>
+                
+                <br />
+
+                
+                <div class="row">
+                <div class="col-md-6">
+                    <h3>Personal Information</h3>
+                    <p><strong>Name:</strong> Daoud Hussain</p>
+                    <p><strong>Phone:</strong> +92 348 3016704</p>
+                    <p><strong>Email:</strong> user@daoudhussain.tech</p>
+                    <p><strong>Website:</strong> daoudhussain.tech</p>
+                </div>
+
+                <div class="col-md-6">
+                    <h3>Working Hours</h3>
+                    <p><strong>Monday - Friday: </strong> 5:00 AM - 12:00 AM</p>
+                    <p><strong>Sunday - Saturday: </strong> 8:00 AM - 10:00 PM</p>
+                </div>
+                </div>
+        
+            </div>
+        
+        
+        </>
     )
 }
 
