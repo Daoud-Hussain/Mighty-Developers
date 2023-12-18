10b927ec047f2635689d561929feb0116911541d
Daoud-Hussain <daoudhussain302@gmail.com>
2023-12-18 18:46:07 +0500

    Contact component completed

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,36 +1,52 @@
 import React from "react";
 
 const Contact = () => {
-    return (
-        <>
-            <div class="container mt-5 text-lg">
-                <h1>Contact Us</h1>
-                <p> Joining a gym is a decision that can change your life for the better. It's about more than just sculpting your physique; it's about building mental fortitude and developing healthy habits that will serve you for a lifetime. Embrace the challenge, push yourself to new heights, and enjoy the satisfaction of knowing that each day in the gym is another step towards becoming the best version of yourself. So join today, and let the journey begin!</p>
-                
-                <br />
+  return (
+    <>
+      <div class="container mt-5">
+        <h1 className="text-center">Contact Us</h1>
+        <h6 >Here's why you should work with us.</h6>
+        <div className="row d-flex flex-row">
+          <div className="col-md-6 ">
+            <ul>
+              <li>Expert work Proficiency</li>
+              <li>Customized Solutions</li>
+              <li>Strong Communication</li>
+              <li>Clean, Reusable Code</li>
+              <li>Timely Delivery</li>
+            </ul>
+          </div>
+          <div class="col-md-6 pt-2">
+            <ul>
+              <li>Cost-Efficiency</li>
+              <li>Client-Centric Approach</li>
+              <li>Usability</li>
+              <li>Post-Launch Support</li>
+            </ul>
+          </div>
+        </div>
+        <br />
 
-                
-                <div class="row">
-                <div class="col-md-6">
-                    <h3>Personal Information</h3>
-                    <p><strong>Name:</strong> Daoud Hussain</p>
-                    <p><strong>Phone:</strong> +92 348 3016704</p>
-                    <p><strong>Email:</strong> user@daoudhussain.tech</p>
-                    <p><strong>Website:</strong> daoudhussain.tech</p>
-                </div>
-
-                <div class="col-md-6">
-                    <h3>Working Hours</h3>
-                    <p><strong>Monday - Friday: </strong> 5:00 AM - 12:00 AM</p>
-                    <p><strong>Sunday - Saturday: </strong> 8:00 AM - 10:00 PM</p>
-                </div>
-                </div>
-        
-            </div>
-        
-        
-        </>
-    )
-}
+        <div class="row">
+          <div class="col-md-6">
+            <h3>Personal Information</h3>
+            <p>
+              <strong>Name:</strong> Daoud Hussain
+            </p>
+            <p>
+              <strong>Phone:</strong> +92 348 3016704
+            </p>
+            <p>
+              <strong>Email:</strong> daoudhussain302@gmail.com
+            </p>
+            <p>
+              <strong>Website:</strong> daoud-hussain.github.io/Portfolio
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Contact;
